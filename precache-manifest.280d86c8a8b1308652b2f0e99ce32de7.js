0f97614176eda35c4882a3138533ae4a9ec4ecdf
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-02-13 18:27:47 +0200

    Updates

---
+++ precache-manifest.280d86c8a8b1308652b2f0e99ce32de7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f893b749ab0d7d53acb91803320653",
+    "url": "/cart-project/index.html"
+  },
+  {
+    "revision": "9a1847a9a75ae28d4576",
+    "url": "/cart-project/static/css/main.ebc1ede0.chunk.css"
+  },
+  {
+    "revision": "d76bede7b664bdd074e7",
+    "url": "/cart-project/static/js/2.f48a34d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cart-project/static/js/2.f48a34d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1847a9a75ae28d4576",
+    "url": "/cart-project/static/js/main.2afecb81.chunk.js"
+  },
+  {
+    "revision": "b0c9d9adb3afaa643bb4",
+    "url": "/cart-project/static/js/runtime-main.768eb91f.js"
+  }
+]);
