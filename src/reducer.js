279f9166dfc7c4ea4ec92c082d506080d28efb41
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-02-13 18:26:34 +0200

    refactor increase and decrease

--- src/reducer.js
+++ src/reducer.js
@@ -8,32 +8,6 @@ const reducer = (state, action) => {
       cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
     };
   }
-  if (action.type === "INCREASE") {
-    let tempCart = state.cart.map((cartItem) => {
-      if (cartItem.id === action.payload) {
-        return { ...cartItem, amount: cartItem.amount + 1 };
-      }
-      return cartItem;
-    });
-    return {
-      ...state,
-      cart: tempCart,
-    };
-  }
-  if (action.type === "DECREASE") {
-    let tempCart = state.cart
-      .map((cartItem) => {
-        if (cartItem.id === action.payload) {
-          return { ...cartItem, amount: cartItem.amount - 1 };
-        }
-        return cartItem;
-      })
-      .filter((cartItem) => cartItem.amount !== 0);
-    return {
-      ...state,
-      cart: tempCart,
-    };
-  }
   if (action.type === "GET_TOTALS") {
     let { total, amount } = state.cart.reduce(
       (cartTotal, cartItem) => {
@@ -59,7 +33,23 @@ const reducer = (state, action) => {
   if (action.type === "DISPLAY_ITEMS") {
     return { ...state, cart: action.payload, loading: false };
   }
-  return state;
+  if (action.type === "TOGGLE_AMOUNT") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          if (action.payload.type === "increase") {
+            return { ...cartItem, amount: cartItem.amount + 1 };
+          }
+          if (action.payload.type === "decrease") {
+            return { ...cartItem, amount: cartItem.amount - 1 };
+          }
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+  throw new Error("no matching action type");
 };
 
 export default reducer;
