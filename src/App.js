09668a485ce794422c2164c7b305c903caf2bb12
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-02-13 14:00:07 +0200

    add reducer and spinner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
+import { ThreeCircles } from "react-loader-spinner";
 
 // components
-import Navbar from './Navbar'
-import CartContainer from './CartContainer'
+import Navbar from "./Navbar";
+import CartContainer from "./CartContainer";
 // items
 
 function App() {
-  // if (loading) {
-  //   return (
-  //     <div className='loading'>
-  //       <h1>Loading...</h1>
-  //     </div>
-  //   )
-  // }
+  const { loading } = useGlobalContext();
+  if (loading) {
+    return (
+      <div className="loading">
+        <ThreeCircles
+          color="#2680c0"
+          height={110}
+          width={110}
+          ariaLabel="three-circles-rotating"
+          wrapperClass="spinner"
+        />
+      </div>
+    );
+  }
   return (
     <main>
       <Navbar />
       <CartContainer />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
